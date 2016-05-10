56fd834af12adace54e3ed98b40ce28786e27e6b
Darkyler <Darkyler57@gmail.com>
2016-05-10 23:44:55 +0200

    Styling courses

--- ngClient/public/js/controllers/courses/courses.controller.js
+++ ngClient/public/js/controllers/courses/courses.controller.js
@@ -4,11 +4,37 @@ myApp.controller("CoursesCtrl", ['$scope','Products',
       $scope.products = [];
       $scope.lowstocks = [];
       $scope.nostocks = [];
+
+      $scope.settingsDisplayed = false;
+      // Alert stock levels
+      $scope.alertLevel3 = 0.1;
+      $scope.alertLevel2 = 0.3;
+      $scope.alertLevel1 = 0.5;
       Products.query(function(result){
         $scope.products = result;
         $scope.getLowStocks();
-
       });
+
+      $scope.toggleSettings = function(){
+        $scope.settingsDisplayed = !$scope.settingsDisplayed;
+      };
+
+      $scope.alertLevel = function(product){
+        if ((product.quantity / product.max) < $scope.alertLevel3 || product.max === 0){
+          product.critical = true;
+          return 'critical';
+        }else if ((product.quantity / product.max) < $scope.alertLevel2 ){
+          product.alertLevel = 2;
+          return 'alert';
+        }else if ((product.quantity / product.max) < $scope.alertLevel1){
+          product.alertLevel = 1;
+          return 'warning';
+        }else {
+          product.alertLevel = 0;
+          return 'cool';
+        }
+      };
+
 
       $scope.getLowStocks = function(){
         for (i=0; i<$scope.products.length; i++){
